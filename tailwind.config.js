5f969493316ba776789193708df23a64b740e011
abdurFaiz <115395387+abdurFaiz@users.noreply.github.com>
2024-01-30 15:20:09 +0700

    dark mode features added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
       center: true,
     },
     extend: {
+      animation: {
+        wiggle: 'wiggle 300ms ease-in-out infinite',
+      },
       keyframes: {
         wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
+          '0%, 100%': { transform: 'rotate(-5deg)' },
+          '50%': { transform: 'rotate(5deg)' },
         }
-      },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
       },
       colors: {
         primary: "#f8fafc",
